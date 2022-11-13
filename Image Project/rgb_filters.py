30d66b64838829b02d9b62c7eaadfc2983860468
benz3927 <benz3927@gmail.com>
2022-11-13 09:55:19 -0500

    Update rgb_filters.py

--- Image Project/rgb_filters.py
+++ Image Project/rgb_filters.py
@@ -31,3 +31,19 @@ def green_filter(image):
             (r,g,b) = image.getpixel((x, y))
             g = g + 140
             image.putpixel((x, y), (r, g, b))
+
+            
+def black_and_white(image):
+    for x in range(image.width):
+        for y in range(image.height):
+            (r,g,b) = image.getpixel((x, y))
+            if r + g + b > 384:
+                r = r + 256
+                g = g + 256
+                b = b + 256
+                
+            else:
+                r = r - 256
+                g = g - 256  
+                b = b - 256
+                image.putpixel((x, y), (r, g, b))
