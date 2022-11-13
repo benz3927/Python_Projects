ca173013b17e5527034dee36321b6ee4580fc3e0
benz3927 <benz3927@gmail.com>
2022-11-13 16:44:48 -0500

    Delete change_brightness.py

--- change_brightness.py
+++
@@ -1,9 +0,0 @@
-def change_brightness(image):
-    answer = int(input("Type in a number between -256 and 256 to make the image darker or brighter respectively! \n"))
-    for x in range(image.width):
-        for y in range(image.height):
-                (r,g,b) = image.getpixel((x, y))
-                r = r + answer
-                g = g + answer
-                b = b + answer
-                image.putpixel((x, y), (r, g, b))
