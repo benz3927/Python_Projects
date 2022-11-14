a352eaca61224485c8e320d2b4fb37d4e679fca0
leonardoli15 <leojames1518@gmail.com>
2022-11-13 21:39:52 -0500

    Create color change.py

---
+++ Image Project/color change.py
@@ -0,0 +1,16 @@
+def color_totals(image):
+    colors = {red:0,
+              green:0,
+              blue:0}
+    for x in range(image.width):
+        for y in range(image.height):
+            (r,g,b) = image.getpixel((x,y))
+            if r>g and r>b:
+                colors[red] = colors[red] + 1
+            elif g>r and g>b:
+                colors[green] = colors[green] + 1
+            elif b>r and b>g:
+                colors[blue] = colors[blue] + 1
+            else:
+                continue
+    return colors
