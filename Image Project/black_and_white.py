093104bd45816799843dbbaf4f013e9e75ab33ce
leonardoli15 <leojames1518@gmail.com>
2022-11-13 21:26:30 -0500

    Update black_and_white.py

--- Image Project/black_and_white.py
+++ Image Project/black_and_white.py
@@ -12,3 +12,13 @@ def black_and_white(image):
                 g = g - 256  
                 b = b - 256
                 image.putpixel((x, y), (r, g, b))
+                
+def black_and_white(image):
+    for x in range(image.width):
+        for y in range(image.height):
+            (r, g, b) = image.getpixel((x,y))
+            if int((r + g + b)/3) > 127:
+                image.putpixel((x,y), (255,255,255))
+            else:
+                image.putpixel((x,y), (0,0,0))
+    return image
