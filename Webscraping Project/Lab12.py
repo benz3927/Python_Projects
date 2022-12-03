d6aac5f3a6c0dad40a803b5fe6984363d67c6df2
benz3927 <benz3927@gmail.com>
2022-12-02 22:21:23 -0500

    Update Lab12.py

--- Webscraping Project/Lab12.py
+++ Webscraping Project/Lab12.py
@@ -24,16 +24,16 @@ def link_getter(movie,parsed_code):
             return movie_url
     
 
-def get_cast(movie_url,parsed_code):
-    cast_url = movie_url + "fullcredits/?ref_=tt_cl_sm"
-    cast_source_code = requests.get(cast_url).text
-    cast_parsed_code = BeautifulSoup(cast_source_code, "html.parser")
+# def get_cast(movie_url,parsed_code):
+#     cast_url = movie_url + "fullcredits/?ref_=tt_cl_sm"
+#     cast_source_code = requests.get(cast_url).text
+#     cast_parsed_code = BeautifulSoup(cast_source_code, "html.parser")
     
 
 # tr, td, then a
 # table cast list, td, tr, a
-    layer1 = parsed_code.find_all("td", class_ = "character")
-    print(layer1)
+#     layer1 = parsed_code.find_all("td", class_ = "character")
+#     print(layer1)
 #     layer2 = layer1[0].find_all("td", class_ = "lister-item-header")
 #     for index in layer2:
 #         a = parsed_code.find("a", class_ = "lister-item-header")
@@ -46,16 +46,17 @@ def main():
     source_code = requests.get(url).text
     parsed_code = BeautifulSoup(source_code, "html.parser")
     
+    everything = parsed_code.find_all("h3", class_ = "lister-item-header")
     
-    everything = parsed_code.find_all("a href", class_ = "lister-item-header")
+#     everything = parsed_code.find_all("a href", class_ = "lister-item-header")
 
     for title in everything:
         print(title.find("a").text)
     
     movie = input('give me a movie name: ')
     movie_url = link_getter(movie,parsed_code)
-    
-    print(get_cast(movie_url,parsed_code))
+#     
+#     print(get_cast(movie_url,parsed_code))
     
 
 
