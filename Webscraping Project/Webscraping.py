261db1abc4da8655062f24b0525f34395579ea81
AChessDonut <czhou@hamilton.edu>
2022-12-04 16:04:12 -0500

    Update Webscraping.py

--- Webscraping Project/Webscraping.py
+++ Webscraping Project/Webscraping.py
@@ -2,6 +2,8 @@
 
 from bs4 import BeautifulSoup
 import requests
+import numpy as np
+import matplotlib.pyplot as plt
 
 def country_counter(url):
     source_code = requests.get(url).text
@@ -37,11 +39,18 @@ def country_counter(url):
         
 def main():
     print(country_counter('https://www.tennisexplorer.com/ranking/atp-men/?page=1)'))
+    
+    countries = ('Spain', 'Norway', 'Greece', 'Serbia', 'Canada', 'Russia', 'USA', 'Poland', 'Denmark', 'Germany', 'Great Britain', 'Italy', 'Croatia', 'Australia', 'Argentina', 'Bulgaria', 'Netherlands', 'Japan', 'Kazakhstan', 'Finland', 'France', 'Slovakia')
+    y_pos = np.arange(len(countries))
+    performance = [6,1,1,2,2,3,9,1,1,1,4,4,2,2,3,1,1,1,1,1,2,1]
+# 
+    plt.bar(y_pos, performance, align='center', alpha=0.5)
+    plt.xticks(y_pos, countries)
+    plt.ylabel('Players')
+    plt.title('Countries in the Top 50')
+    
+    plt.show()
 
     
-       
-    
-
-
 main()
 
